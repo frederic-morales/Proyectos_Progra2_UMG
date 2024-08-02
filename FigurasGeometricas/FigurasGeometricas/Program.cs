45451701098b577f2df6ef861a8231ec667fc928
frederic-morales <tocomorales30@gmail.com>
2024-08-01 19:48:10 -0600

    Creando clases de figuras geometricas

---
+++ FigurasGeometricas/FigurasGeometricas/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FigurasGeometricas;
+
+Triangulo triangulo = new Triangulo();
+triangulo.Nombre = "Primer Triangulo";
+triangulo.Clasificacion = "isosceles";
+triangulo.Base = 8.3;
+triangulo.Altura = 14.1;
+
+Console.WriteLine(triangulo.area);
---
+++ FigurasGeometricas/FigurasGeometricas/Triangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasGeometricas
+{
+    public class Triangulo
+    {
+        public string Nombre;
+        public int Lados = 3;
+        public string Clasificacion;
+        public double Base;
+        public double Altura;
+        public double perimetro;
+        public double area;
+
+        private void calcularArea()
+        {
+            area = (Base * Altura) / 2;
+        }
+    }
+
+    public class Cuadrado
+    {
+
+    }
+}
